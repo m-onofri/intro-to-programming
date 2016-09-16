f821cbd1af5a4c50e3fa824d1b8420eeafa196ec
Manuele Onofri <manuele.onofri@gmail.com>
2016-09-16 10:03:57 +0200

    Complete variables exercises

---
+++ 02_variables_exercise/variables_05-06_ex.rb
@@ -0,0 +1,9 @@
+# Exercise 5
+# In the first program x = 3
+# In the second program you get an error "undefined local variable or method",
+# because x is not available outside the do/end block of times method.
+
+# Exercise 6
+# It tells me that the program is trying to call a variable called shoes,
+# that either has not already been defined in the program or has been defined
+# outside the scope in which the variable is called.
